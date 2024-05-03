f77357e25d2e6990206b8331a4fcfcbf300f6e64
dayeongkim999 <dayeongkim999@gmail.com>
2024-05-03 16:11:06 +0900

    When click save button, print UserInput through log.d

--- app/src/main/java/com/example/calendar/SecondFragment.kt
+++ app/src/main/java/com/example/calendar/SecondFragment.kt
@@ -1,12 +1,16 @@
 package com.example.calendar
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import androidx.navigation.fragment.findNavController
 import com.example.calendar.databinding.FragmentSecondBinding
+import java.util.Calendar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -31,6 +35,27 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnCheckedChangeListener{ buttonView, isChecked->
+            if(isChecked)
+                binding.calendar.visibility = View.GONE;
+            else
+                binding.calendar.visibility = View.VISIBLE
+        }
+
+        binding.savebutton.setOnClickListener{
+            val placeText: String = binding.placetext.text.toString()
+            val alarmtimeText: String = binding.alarmtimetext.text.toString()
+            val emailText: String = binding.emailtext.text.toString()
+            val memoText: String = binding.memotext.text.toString()
+            val timeText: String = binding.timetext.text.toString()
+            val repeaText: String = binding.repeattext.text.toString()
+            Log.d("UserInput", placeText)
+            Log.d("UserInput", alarmtimeText)
+            Log.d("UserInput", emailText)
+            Log.d("UserInput", memoText)
+            Log.d("UserInput", timeText)
+            Log.d("UserInput", repeaText)
+        }
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
